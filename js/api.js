33ff9931e7274e71b4dc9445aa84d7adfde1b4c0
Hily <164035013+HilaryTorres@users.noreply.github.com>
2024-05-02 13:26:10 -0400

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+
+  $.get('http://fakestoreapi.com/products', function(data) {
+    $('#fila-ropa').empty();
+    $.each(data, function(i, item) {
+      var fila = `
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 justify-content-center">
+              <div class="card mb-4" style="margin:auto">
+                <img src="${item.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${item.title}</h5>
+                  <p class="card-text">${item.category}</p>
+                  <br>
+                  <p class="card-text">${item.description}</p>
+                  <p class="card-text, precio">Precio: ${item.price}</p>      
+                  <p class="card-text">Stock disponible: ${item.rating.count}</p>
+                </div>
+              </div>
+            </div> 
+      `;
+      $('#fila-ropa').append(fila);
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
